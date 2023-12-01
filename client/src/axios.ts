4c9019f2e373d483a1ec5a6a51953a3c37158f03
fefiy <foziayimam87@gmail.com>
2023-12-01 09:28:03 +0300

    correct url

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios"
-
+const baseUrl = "https://node-type-music.onrender.com/api"
 export const makeRequest = axios.create({
-    baseURL:"http://localhost:3007/api"
+    baseURL:baseUrl
 })
 
 export const privateAxios = axios.create({
-    baseURL: "http://localhost:3007/api",
+    baseURL: baseUrl,
     headers: { 'Content-Type': 'application/json' },
   });
